81eb36da5034304b1b18227cc8471e2bfacecc6f
smazik02 <s.mazik2002@gmail.com>
2025-05-06 14:59:01 +0200

    permanent redirect with /auth

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-    /* config options here */
+    async redirects() {
+        return [
+            {
+                source: '/auth',
+                destination: '/auth/sign-in',
+                permanent: true,
+            },
+        ];
+    },
 };
 
 export default nextConfig;
